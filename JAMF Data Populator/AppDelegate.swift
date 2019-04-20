f8d155c707bb2cf665e922f98e0b61e926a49e07
Jeremy Matthews <jeremymatthews@mac.com>
2019-04-20 16:24:03 -0700

    Initial Commit

---
+++ JAMF Data Populator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JAMF Data Populator
+//
+//  Created by Jeremy Matthews on 4/20/19.
+//  Copyright © 2019 SISU Works. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JAMF Data Populator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JAMF Data Populator
+//
+//  Created by Jeremy Matthews on 4/20/19.
+//  Copyright © 2019 SISU Works. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
