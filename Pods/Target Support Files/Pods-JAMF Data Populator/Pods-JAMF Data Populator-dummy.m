0d0665d083a3ae57a9bbce7427992f6fbfc6a0b4
Jeremy Matthews <jeremymatthews@mac.com>
2019-07-19 16:45:41 -0700

    capturing garbage files for processing later

---
+++ Pods/Target Support Files/Pods-JAMF Data Populator/Pods-JAMF Data Populator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JAMF_Data_Populator : NSObject
+@end
+@implementation PodsDummy_Pods_JAMF_Data_Populator
+@end
---
+++ Pods/Target Support Files/Pods-JAMF Data PopulatorUITests/Pods-JAMF Data PopulatorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JAMF_Data_PopulatorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_JAMF_Data_PopulatorUITests
+@end
